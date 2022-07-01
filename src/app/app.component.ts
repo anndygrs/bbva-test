fce5ca1eb5f7edb68ad53c50e34e02ecb5ec2518
Andrea García <andreagarcia_sanchez@hotmail.com>
2022-07-01 06:36:03 -0500

    Information rendering. Need to adjust styles.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
@@ -7,7 +7,9 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
 
-  readInfo(info: any) {
-    console.log(info);
+  info: any;
+  evidence: any;
+  readInfo(facts: any) {
+    this.info = facts;
   }
 }
--- src/app/render/render.component.ts
+++ src/app/render/render.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 
 @Component({
   selector: 'app-render',
@@ -7,9 +7,22 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RenderComponent implements OnInit {
 
+  name: String = '';
+  state: String = '';
+  hour: String = '';
+  imecaValue: String = '';
+
+  @Input() evidence: any;
   constructor() { }
 
   ngOnInit(): void {
+    this.name = this.evidence['stations'][0]['name'];
+    if(this.name != 'station'){
+      this.state = this.evidence['stations'][0]['source_id'];
+      this.hour = this.evidence['stations'][0]['indexes'][0]['calculationTime'];
+      this.imecaValue = this.evidence['stations'][0]['indexes'][0]['value'];
+    } else {
+      this.name = '';
+    }
   }
-
 }

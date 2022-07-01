ced8b7b5bf3814cd7e604fbd006c7de41cf27fe9
Andrea García <andreagarcia_sanchez@hotmail.com>
2022-07-01 03:52:21 -0500

    First commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,14 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { DataComponent } from './data/data.component';
+import { RenderComponent } from './render/render.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DataComponent,
+    RenderComponent
   ],
   imports: [
     BrowserModule
